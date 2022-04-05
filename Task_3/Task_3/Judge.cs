06bfae311c4b988ab613192f4dab24e1165c31d4
DenisKolosovskij <den.kolosovskij@mail.ru>
2022-04-05 23:10:16 +0300

    <Task_3>

---
+++ Task_3/Task_3/Judge.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GeneralizedRockPaperScissors
+{
+    enum Outcome
+    {
+        WIN,
+        LOSE,
+        DRAW
+    }
+    class Judge
+    {
+        int MovesCount;
+
+        public Judge(int movesCount)
+        {
+            MovesCount = movesCount;
+        }
+
+        public Outcome Decide(int firstMove, int secondMove)
+        {
+            if (firstMove == secondMove)
+            {
+                return Outcome.DRAW;
+            }
+
+            if ((secondMove > firstMove && secondMove - firstMove > MovesCount / 2) || (secondMove < firstMove && firstMove - secondMove <= MovesCount / 2))
+            {
+                return Outcome.WIN;
+            }
+
+            return Outcome.LOSE;
+        }
+    }
+}
---
+++ Task_3/Task_3/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Linq;
+using System.Security.Cryptography;
+
+namespace GeneralizedRockPaperScissors
+{
+    class Program
+    {
+        static bool CheckArgs(string[] args)
+        {
+            if (args.Length < 3 || args.Length % 2 == 0)
+            {
+                Console.WriteLine("Invalid options");
+                return false;
+            }
+
+            if (args.Length != args.Distinct().Count())
+            {
+                Console.WriteLine("Invalid options");
+                return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            args = new[] { "qwe", "rty", "uio", "asd", "zxc" };
+            if (!CheckArgs(args))
+            {
+                return;
+            }
+
+            var sec = new Security();
+            var a = new Table(args);
+            var judge = new Judge(args.Length);
+
+            bool gameFinished = false;
+
+            while (!gameFinished)
+            {
+                var key = sec.GenerateKey();
+                var computerMove = RandomNumberGenerator.GetInt32(args.Length);
+                var hmac = sec.GenerateHMAC(key, args[computerMove]);
+
+                Console.WriteLine("HMAC: " + hmac);
+
+                Console.WriteLine("Available Moves:");
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(i + 1 + " - " + args[i]);
+                }
+                Console.WriteLine("0 - Exit");
+                Console.WriteLine("? - Help");
+
+                Console.Write("Enter your move: ");
+                var ans = Console.ReadLine();
+
+                if (ans == "?")
+                {
+                    a.Print();
+                    Console.Write("\n\n\n");
+                    continue;
+                }
+
+                if (ans == "0")
+                {
+                    gameFinished = true;
+                    continue;
+                }
+
+                int playerMove = 0;
+
+                if (!int.TryParse(ans, out playerMove) || playerMove <= 0 || playerMove > args.Length)
+                {
+                    Console.Write("\n\n\n");
+                    continue;
+                }
+
+                Console.WriteLine("Your move: " + args[playerMove - 1]);
+                Console.WriteLine("Computer move: " + args[computerMove]);
+
+                switch (judge.Decide(computerMove, playerMove - 1))
+                {
+                    case Outcome.WIN:
+                        Console.WriteLine("You win!");
+                        break;
+
+                    case Outcome.LOSE:
+                        Console.WriteLine("You lost!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Draw!");
+                        break;
+                }
+
+                Console.WriteLine("HMAC key: " + key);
+                Console.Write("\n\n\n");
+            }
+        }
+    }
+}
